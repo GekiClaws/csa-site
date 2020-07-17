64e8a9aaf3253c9ee9a638205f9d210934339c83
Matthew Wong <mattckwong@gmail.com>
2020-07-17 18:04:08 +0800

    Updated product pages
    Changed some images into html diagrams

--- frontend/cs/script.js
+++ frontend/cs/script.js
@@ -1,37 +1,37 @@
-// // Carousel 1
-// $('#carouselExample').on('slide.bs.carousel', function (e) {
+// Carousel 1
+$('#carouselExample').on('slide.bs.carousel', function (e) {
 
-//     /*
+    /*
 
-//     CC 2.0 License Iatek LLC 2018
-//     Attribution required
+    CC 2.0 License Iatek LLC 2018
+    Attribution required
     
-//     */
+    */
 
-//     var $e = $(e.relatedTarget);
+    var $e = $(e.relatedTarget);
     
-//     var idx = $e.index();
-//     console.log("IDX :  " + idx);
+    var idx = $e.index();
+    console.log("IDX :  " + idx);
     
-//     var itemsPerSlide = 6;
-//     var totalItems = $('.carousel-item').length;
+    var itemsPerSlide = 6;
+    var totalItems = $('.carousel-item').length;
     
-//     if (idx >= totalItems-(itemsPerSlide-1)) {
-//         var it = itemsPerSlide - (totalItems - idx);
-//         for (var i=0; i<it; i++) {
-//             // append slides to end
-//             if (e.direction=="left") {
-//                 $('.carousel-item').eq(i).appendTo('.carousel-inner');
-//             }
-//             else {
-//                 $('.carousel-item').eq(0).appendTo('.carousel-inner');
-//             }
-//         }
-//     }
-// });
+    if (idx >= totalItems-(itemsPerSlide-1)) {
+        var it = itemsPerSlide - (totalItems - idx);
+        for (var i=0; i<it; i++) {
+            // append slides to end
+            if (e.direction=="left") {
+                $('.carousel-item').eq(i).appendTo('.carousel-inner');
+            }
+            else {
+                $('.carousel-item').eq(0).appendTo('.carousel-inner');
+            }
+        }
+    }
+});
 
 // Product slideshow
-var slideIndex = 1;
+var slideIndex = 2;
 
 window.addEventListener("load", function() {
     showSlides(slideIndex);
